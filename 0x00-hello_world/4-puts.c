bce54008198171162f726e05199854e5bd5eb3e8
Jortijosh <jortijosh@gmail.com>
2023-07-31 06:26:18 -0700

    late

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+ #include <stdio.h>
 /**
  * main - main entry point
- * return ; always 0 (success)
+ * return : always 0 (success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle")
-	return (0);
+	;return (0);
+	;
 }
